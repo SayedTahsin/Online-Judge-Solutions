12289a051965ee628d9689811cb7c2636d4915a6
SayedTahsin <mail.tahsin99@gmail.com>
2023-03-25 22:19:25 +0600

    F_Bouncy_Ball

--- F_Bouncy_Ball.cpp
+++ F_Bouncy_Ball.cpp
@@ -57,14 +57,12 @@ using o_map = tree<DT1, DT2, FUNC, rb_tree_tag, tree_order_statistics_node_updat
 #define lcm(x, y) x *(y / gcd(x, y))
 #define preci(x) fixed << setprecision(x)
 #define PI (acos(-1.0))
-#define theta(x) x *acos(-1.0) / 180.0     /// degree to radian
-#define thetainv(x) x * 180.0 / acos(-1.0) /// radian to degree
 #define SZ(x) (int)x.size()
 #define bug cout << "*_*\n"
 //!-------
 #define EPS (1e-6)
 const ll INF = 1e18 + 5;
-const ll MOD = 998244353;
+const ll MOD = 1e9 + 7;
 bool equalTo(double a, double b) { return ((fabs(a - b) <= EPS) ? true : false); }
 bool notEqual(double a, double b) { return ((fabs(a - b) > EPS) ? true : false); }
 bool lessThan(double a, double b) { return ((a + EPS < b) ? true : false); }
@@ -118,7 +116,7 @@ template <class T>
 inline void print(T u) { cout << '*' << u << '*' << endl; }
 //!---------
 int cs = 1;
-inline void CASE() { cout << "Case " << cs++ << ": "; }
+inline void CASE() { cout << "Case #" << cs++ << ": "; }
 inline int ciel(double a, double b) { return (a + (b - 1)) / b; }
 inline int numOfDigit(int n) { return log10(n) + 1; }
 inline int bitsInBinary(int n) { return log2(n) + 1; }
@@ -133,38 +131,63 @@ bool is_prime(ll n)
             return false;
     return true;
 }
-bool is_pel(string s)
-{
-    ll n = s.length() - 1;
-    for (int i = 0; i < s.length() / 2; i++, n--)
-        if (s[i] != s[n])
-            return false;
-    return true;
-}
-string to_binary(ll a)
-{
-    string s;
-    while (a)
-    {
-        s = ((a % 2) ? "1" : "0") + s;
-        a /= 2;
-    }
-    return s.empty() ? "0" : s;
-}
 
 void solve()
 {
+    int n, m, x1, x2, y1, y2;
+    string s;
+    cin >> n >> m >> x1 >> y1 >> x2 >> y2 >> s;
+    // set<pair<int, pair<int, string>>> mp;
+    // set<pii> st;
+    int up = 0, dw = 0;
+    if (s[0] == 'D')
+        up = 1;
+    if (s[1] == 'R')
+        dw = 1;
+    int ans=0;
+    map<pair<pii,pii>,int>mp;
+    while (1)
+    {
+        if (x1 == x2 && y1 == y2)
+            break;
+        int tmpx=x1,tmpy=y1;
+        if (up) x1++;
+        else x1--;
+        if (dw) y1++;
+        else y1--;
+        int f=0;
+        if(x1<1){
+            x1=2,up=1,ans++,f++;
+        }else if(x1>n){
+            x1=n-1,up=0,ans++,f++;
+        }
+        if(y1<1){
+            y1=2,dw=1;
+            if(!f)ans++;
+        }else if(y1>m){
+            y1=m-1,dw=0;
+            if(!f)ans++;
+        }
+        if (x1 == x2 && y1 == y2)
+            break;
+        mp[{{tmpx,tmpy},{x1,x2}}]++;
+        if(mp[{{tmpx,tmpy},{x1,x2}}]>4){
+            cout<<-1<<endl;
+            return;
+        }
+    }
+    cout<<ans<<endl;
 }
+
 main()
 {
 #ifndef ONLINE_JUDGE
     freopen("D:/Entertainment/code/C++/OJ/in.txt", "r", stdin);
     freopen("D:/Entertainment/code/C++/OJ/out.txt", "w", stdout);
 #endif
-
     NFS;
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while (t--)
         solve();
 }
