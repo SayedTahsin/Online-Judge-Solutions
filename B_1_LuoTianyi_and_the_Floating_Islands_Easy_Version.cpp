3d5e4c3bf36959e3b122bc66ee3a90ebf92ee33e
SayedTahsin <mail.tahsin99@gmail.com>
2023-05-23 16:37:27 +0600

    5/23/2023 4:37PM

--- B_1_LuoTianyi_and_the_Floating_Islands_Easy_Version.cpp
+++ B_1_LuoTianyi_and_the_Floating_Islands_Easy_Version.cpp
@@ -57,14 +57,12 @@ using o_map = tree<DT1, DT2, FUNC, rb_tree_tag, tree_order_statistics_node_updat
 #define lcm(x, y) x *(y / gcd(x, y))
 #define preci(x) fixed << setprecision(x)
 #define PI (acos(-1.0))
-#define theta(x) x *acos(-1.0) / 180.0     /// degree to radian
-#define thetainv(x) x * 180.0 / acos(-1.0) /// radian to degree
 #define SZ(x) (int)x.size()
 #define bug cout << "*_*\n"
 //!-------
 #define EPS (1e-6)
 const ll INF = 1e18 + 5;
-const ll MOD = 1000000007;
+const ll MOD = 1e9 + 7;
 bool equalTo(double a, double b) { return ((fabs(a - b) <= EPS) ? true : false); }
 bool notEqual(double a, double b) { return ((fabs(a - b) > EPS) ? true : false); }
 bool lessThan(double a, double b) { return ((a + EPS < b) ? true : false); }
@@ -97,8 +95,8 @@ inline ll modSub(ll a, ll b) { return mod(mod(a) - mod(b)); }
 inline ll modMul(ll a, ll b) { return mod(mod(a) * mod(b)); }
 inline ll modInv(ll a) { return bin_expo(a, MOD - 2); }
 //!---------
-int dx[] = {0, 0, -1, 1};
-int dy[] = {1, -1, 0, 0};
+// int dx[] = {0, 0, -1, 1};
+// int dy[] = {1, -1, 0, 0};
 // int dxk[] = {-2, -2, -1, -1, 1, 2, 2, 1};
 // int dyk[] = {1, -1, 2, -2, 2, 1, -1, -2};
 // int dx1[] = {0, 1, 1, 1, 0, -1, -1, -1};
@@ -118,7 +116,7 @@ template <class T>
 inline void print(T u) { cout << '*' << u << '*' << endl; }
 //!---------
 int cs = 1;
-inline void CASE() { cout << "Case " << cs++ << ": "; }
+inline void CASE() { cout << "Case #" << cs++ << ": "; }
 inline int ciel(double a, double b) { return (a + (b - 1)) / b; }
 inline int numOfDigit(int n) { return log10(n) + 1; }
 inline int bitsInBinary(int n) { return log2(n) + 1; }
@@ -133,60 +131,38 @@ bool is_prime(ll n)
             return false;
     return true;
 }
-bool is_pel(string s)
-{
-    ll n = s.length() - 1;
-    for (int i = 0; i < s.length() / 2; i++, n--)
-        if (s[i] != s[n])
-            return false;
-    return true;
-}
-int MEX(vector<int> &v)
-{
-    set<int> s;
-    fore(v) s.insert(x);
-    int m = 0;
-    fore(s)
-    {
-        if (x == m)
-            m++;
-        else
-            break;
-    }
-    return m;
-}
-string shiftStringRight(string s, int c)
-{
-    string x;
-    for (int i = s.length() - c; i < s.length(); i++)
-        x += s[i];
-    for (int i = 0; i < s.length() - c; i++)
-        x += s[i];
-    return x;
-}
-
-string shiftStringLeft(string s, int c)
-{
-    string x;
-    for (int i = c; i < s.length(); i++)
-        x += s[i];
-    for (int i = 0; i < c; i++)
-        x += s[i];
-    return x;
-}
-//!-------------------------------------------
 
 void solve()
 {
-    
+    int n, k;
+    cin >> n >> k;
+    for (int i = 1; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+    if (k == 1 || k == 3)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        int total = (n * (n - 1)) / 2;
+        int s = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            s += (i * (n - (i - 1))) % MOD;
+        }
+        cout << modMul(s, modInv(total)) << endl;
+    }
 }
+
 main()
 {
 #ifndef ONLINE_JUDGE
     freopen("D:/Entertainment/code/C++/OJ/in.txt", "r", stdin);
     freopen("D:/Entertainment/code/C++/OJ/out.txt", "w", stdout);
 #endif
-
     NFS;
     int t = 1;
     // cin >> t;
--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,78 +1,14 @@
-/// Bismillahir Rahmanir Raheem
 #include <bits/stdc++.h>
-#include <algorithm>
 using namespace std;
 
-string a, b;
-int n, m;
-int dp[3000][3000];
-int dr[3000][3000];
-int f(int i, int j)
-{
-    if (i == n || j == m)
-        return 0;
 
-    if (dp[i][j] != -1)
-        return dp[i][j];
 
-    int ret;
-    if (a[i] == b[j])
-    {
-        ret = 1 + f(i + 1, j + 1);
-        dr[i][j] = 1;
-    }
-    else
-    {
-        int a = f(i + 1, j);
-        int b = f(i, j + 1);
-        if (a > b)
-        {
-            ret = a;
-            dr[i][j] = 2;
-        }
-        else
-        {
-            ret = b;
-            dr[i][j] = 3;
-        }
-    }
-    return dp[i][j] = ret;
-}
-string ans;
-void printdr(int i, int j)
-{
-    if (i == n || j == m)
-        return;
-
-    if (dr[i][j] == 1)
-    {
-        ans += a[i];
-        printdr(i + 1, j + 1);
-    }
-    else if (dr[i][j] == 2)
-        printdr(i + 1, j);
-    else if (dr[i][j] == 3)
-        printdr(i, j + 1);
-}
-void solve()
-{
-    memset(dp, -1, sizeof(dp));
-    cin >> a >> b;
-    n = a.length();
-    m = b.length();
-    f(0, 0);
-    printdr(0, 0);
-    cout << ans << endl;
-}
 int main()
 {
-    // #ifndef ONLINE_JUDGE
-    //     freopen("D:/code/C++/OJ/in.txt", "r", stdin);
-    //     freopen("D:/code/C++/OJ/out.txt", "w", stdout);
-    // #endif
-    int t = 1;
-    while (t--)
-    {
-        solve();
-    }
+#ifndef ONLINE_JUDGE
+    freopen("D:/Entertainment/code/C++/OJ/in.txt", "r", stdin);
+    freopen("D:/Entertainment/code/C++/OJ/out.txt", "w", stdout);
+#endif
+
+    cout << f(5);
 }
